8e37b6a53534491aaa73f7a35fa6824ee35f39ed
giramos <UO202549@uniovi.es>
2025-02-04 11:49:56 +0100

    :four: preparacion entorno API :four:

---
+++ API/CustomersApi/CustomersApi/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using CustomersApi.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomersApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        //api/customer/
+        [HttpGet]
+        public async Task<CustomerDto> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+        //api/customer/{id}
+        [HttpGet("{id}")]
+        public async Task<CustomerDto> GetCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost]
+        public async Task<CustomerDto> CreateCustomer(CreateCustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<CustomerDto> UpdateCustomer(CustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ API/CustomersApi/CustomersApi/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(); // añade el controlador que hemos creado
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build(); //  construye una app
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) // swagger solo desarrollo
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// ESTO es lógica. No se recomienda ponerlo aqui
+//app.MapGet("/customer/{id}", (long id) =>
+//{
+//    return "net 6";
+//});
+
+app.MapControllers();
+
+app.Run();
