669491fa7aec688ee60d9ec2a9d4b366a5fce46b
giramos <UO202549@uniovi.es>
2025-02-04 12:46:08 +0100

    :five: validadores y ruta a minusculas :five:

--- API/CustomersApi/CustomersApi/Dtos/CreateCustomerDto.cs
+++ API/CustomersApi/CustomersApi/Dtos/CreateCustomerDto.cs
@@ -1,9 +1,15 @@
-﻿namespace CustomersApi.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomersApi.Dtos
 {
     public class CreateCustomerDto
     {
+        [Required (ErrorMessage = "El nombre propio tiene que estar especificado")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "El apellido propio tiene que estar especificado")]
         public string LastName { get; set; }
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "El formato del email no es válido.")]
         public string Email { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
--- API/CustomersApi/CustomersApi/Program.cs
+++ API/CustomersApi/CustomersApi/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers(); // añade el controlador que hemos creado
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(routing => routing.LowercaseUrls = true); // minuscula en la ruta . De https://localhost:7163/api/Customer a https://localhost:7163/api/customer
+
 
 var app = builder.Build(); //  construye una app
 
